6c2085cac22c9b52d8ab333b652e68fc86c19a87
phani7 <phani732848@gmail.com>
2023-06-28 14:23:12 +0530

    js basics

---
+++ index.js
@@ -0,0 +1,17 @@
+const number = 50
+
+function print() {
+  const square = number * number
+
+  if (number < 60) {
+    var largerNumber = 80
+    const anotherLargerNumber = 100
+
+    console.log(square)
+  }
+
+  console.log(largerNumber)
+  console.log(anotherLargerNumber)
+}
+
+print()
